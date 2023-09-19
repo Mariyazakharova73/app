c940efcef049f40805f8b9436f51e5b79e3f8159
Mariya Zakharova <zakharovamaria73@yandex.ru>
2023-09-19 13:13:33 +0400

    yml

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -10,19 +10,19 @@ export default ({ config }: { config: webpack.Configuration }) => {
     entry: '',
     html: '',
   };
-  config?.resolve?.modules?.push(paths.src);
-  config?.resolve?.extensions?.push('.ts', '.tsx');
-// @ts-ignore
-  config?.module?.rules = config?.module?.rules?.map((rule:webpack.RuleSetRule) => {
+  config.resolve.modules.push(paths.src);
+  config.resolve.extensions.push('.ts', '.tsx');
+
+  config.module.rules = config.module.rules.map((rule:webpack.RuleSetRule) => {
     if (/svg/.test(rule.test as string)) {
       return { ...rule, exclude: /\.svg$/i };
     }
     return rule;
   });
-  config?.module?.rules?.push({
+  config.module.rules.push({
     test: /\.svg$/,
     use: ['@svgr/webpack'],
   });
-  config?.module?.rules?.push(buildCssLoaders(true));
+  config.module.rules.push(buildCssLoaders(true));
   return config;
 };
